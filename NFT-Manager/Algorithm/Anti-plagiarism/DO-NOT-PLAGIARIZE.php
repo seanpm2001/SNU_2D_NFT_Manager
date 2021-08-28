e33ca452910d6d67a10fb5b10eaead6feea740bd
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-27 18:11:11 -0700

    DO-NOT-PLAGIARIZE.php
    The anti-plagiarism script for this project.

---
+++ NFT-Manager/Algorithm/Anti-plagiarism/DO-NOT-PLAGIARIZE.php
@@ -0,0 +1,17 @@
+<?php
+// DO-NOT-PLAGIARIZE.php
+// This script is to protect against users plagiarizing NF tokens
+$PLAGIARIZATION_DETECTION {
+  // I don't know how to do this yet, so I am leaving it blank
+  echo ("Failed to initialize plagiarism detection script");
+  break;
+}
+return PLAGIARIZATION_DETECTION();
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Friday, August 27th 2021 at 6:11 pm)
+* Line count (including blank lines and compiler line): 18
+*/
+// I am not producing good works, I will stop.
+?>
