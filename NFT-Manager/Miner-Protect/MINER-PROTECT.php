34f2a8c4f1f4d0658d401d794f5d8ceb44ac9c84
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-27 18:04:09 -0700

    MINER-PROTECT.php
    The miner protection script for this project.

---
+++ NFT-Manager/Miner-Protect/MINER-PROTECT.php
@@ -0,0 +1,16 @@
+<?php
+// Miner Protect.php
+// This script is to protect the user from cryptocurrency miners from accessing their browser
+$MINER_PROTECT {
+  // I don't know how to do this yet, so I am leaving it blank
+  echo ("Miner protect is not yet configured. You may not be protected from cryptocurrency miner processes");
+  break;
+}
+return MINER_PROTECT();
+break;
+/* File info
+* File type: PHP Source file (*.php)
+* File version: 1 (Friday, August 27th 2021 at 6:04 pm)
+* Line count (including blank lines and compiler line): 17
+*/
+?>
